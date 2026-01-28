17bd9a0686c9826571153720a3c4d4aa8bc8972a
Oilen <63976349+Oilenn@users.noreply.github.com>
2026-01-28 18:30:44 +0400

    feat: add base db context

---
+++ KidsOrganizationApp/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using KidsOrganizationApp.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace KidsOrganizationApp
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Parent> Parents => Set<Parent>();
+        public DbSet<Child> Children => Set<Child>();
+        public DbSet<PersonInfo> PersonInfos => Set<PersonInfo>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=kids_org.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+    }
+}
