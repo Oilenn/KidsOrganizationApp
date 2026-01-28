0111f4092db9a88693547a1935dc4b14f4e26374
Oilen <63976349+Oilenn@users.noreply.github.com>
2026-01-28 18:28:01 +0400

    feat: add view to test

---
+++ KidsOrganizationApp/UI/View/ParentChildView.xaml.cs
@@ -0,0 +1,29 @@
+﻿using KidsOrganizationApp.UI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KidsOrganizationApp.UI.View
+{
+    public partial class ParentChildView : Window
+    {
+        public AppDbContext Db;
+        public ParentChildView()
+        {
+            Db = new AppDbContext();
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+    }
+}
