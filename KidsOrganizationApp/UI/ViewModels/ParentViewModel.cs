8a5ebe3c2be8db809b5b343906da6e43f4b89d13
Oilen <63976349+Oilenn@users.noreply.github.com>
2026-01-28 18:27:25 +0400

    feat: add base ViewModels

---
+++ KidsOrganizationApp/UI/ViewModels/ParentViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidsOrganizationApp.UI.ViewModels
+{
+    public class ParentViewModel
+    {
+        public Guid Id { get; }
+        public string Name { get; }
+        public string Surname { get; }
+        public string Patronomic { get; }
+        public string DateOfBirth { get; }
+
+        public ParentViewModel(Domain.Parent parent)
+        {
+            Id = parent.Id;
+            Name = parent.Name;
+            Surname = parent.Surname;
+            Patronomic = parent.Patronymic;
+            DateOfBirth = parent.DateBirth.ToString("dd.MM.yyyy");
+        }
+    }
+}
