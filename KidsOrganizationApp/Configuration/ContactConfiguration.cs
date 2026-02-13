65f2289c1cc60a4641ee96863a1c7b6e4d748d27
Oilen <Oilenn@users.noreply.github.com>
2026-02-14 01:38:48 +0400

    feat: ready configurations

---
+++ KidsOrganizationApp/Configuration/ContactConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using KidsOrganizationApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KidsOrganizationApp.Infrastructure.Configurations
+{
+    public class ContactConfiguration : IEntityTypeConfiguration<Contact>
+    {
+        public void Configure(EntityTypeBuilder<Contact> builder)
+        {
+            builder.ToTable("Contacts");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Id)
+                   .ValueGeneratedNever();
+
+            builder.Property(c => c.MobileNumber)
+                   .IsRequired()
+                   .HasMaxLength(11);
+
+            builder.Property(c => c.LivingPlace)
+                   .IsRequired()
+                   .HasMaxLength(255);
+        }
+    }
+}
---
+++ KidsOrganizationApp/Configuration/DocumentConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using KidsOrganizationApp.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KidsOrganizationApp.Infrastructure.Configurations
+{
+    public class DocumentConfiguration : IEntityTypeConfiguration<Document>
+    {
+        public void Configure(EntityTypeBuilder<Document> builder)
+        {
+            builder.ToTable("Documents");
+
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.Id)
+                   .ValueGeneratedNever();
+
+            // Enum хранится как int (по умолчанию)
+            builder.Property(d => d.Type)
+                   .IsRequired()
+                   .HasConversion<int>();
+
+            builder.Property(d => d.Path)
+                   .IsRequired()
+                   .HasMaxLength(500);
+        }
+    }
+}
